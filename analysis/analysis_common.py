c615cb4657e4fa7c134b5cf6717e6dbd6bba3f81
Alex Barcelo <alex@betarho.net>
2020-11-29 18:45:26 +0100

    operational version of analysis scripts and image generation

--- analysis/analysis_common.py
+++ analysis/analysis_common.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from matplotlib.legend_handler import HandlerTuple
 from matplotlib import ticker
+from matplotlib.legend_handler import HandlerTuple
+from matplotlib.transforms import Bbox
 from pandas_ods_reader import read_ods
 
 def plt_clean_legend():
@@ -76,16 +77,33 @@ def kernel_plot_tweaks(g, factor, legend_title="", rotate=False):
     ax2 = g.twinx()
     ax2.set_ylabel("big objects kernel execution time (s)")
     ax2.set_ylim(0, max_value * factor)
+    
+    leg = g.get_legend()
+    color0 = leg.legendHandles[0]._facecolor
+    color1 = leg.legendHandles[1]._facecolor
+    
+    def dark_color(x, s):
+        return x[0]*s, x[1]*s, x[2]*s, x[3]
+
+    ax2.spines['left'].set_color(color0)
+    g.tick_params(axis='y', colors=dark_color(color0, 0.5))
+    g.yaxis.label.set_color(dark_color(color0, 0.5))
+    
+    ax2.spines['right'].set_color(color1)
+    ax2.tick_params(axis='y', colors=dark_color(color1, 0.6))
+    ax2.yaxis.label.set_color(dark_color(color1, 0.6))
 
 
 def xlabel_tweaks(g, n_active, n_non_active, rotate=False):
     g.set_xlabel("")
+    g.set_ylabel("execution time (s)")
     if rotate:
         g.set_xticklabels(g.get_xticklabels(), rotation=90, horizontalalignment='right')
         position=-0.6
         ticklength = 50
     else:
-        position=-0.2
+        g.set_xticklabels(g.get_xticklabels(), rotation=30, horizontalalignment='center')
+        position=-0.5
         ticklength = 25
 
     total = n_active + n_non_active
@@ -102,3 +120,12 @@ def xlabel_tweaks(g, n_active, n_non_active, rotate=False):
     ax2.tick_params(which='minor', width=0, length=0, labelsize=10)
     ax2.tick_params(which='major', width=1, length=ticklength, labelsize=10)
     ax2.set_xticks(xticks)
+
+
+def save_tweaks(filename):
+    size = (10, 4)
+    plt.gcf().set_size_inches(size[0], size[1])
+    b = plt.gcf().get_tightbbox(plt.gcf().canvas.get_renderer())
+    bbox = Bbox.from_bounds(b.x0, b.y0+0.3, b.x1-b.x0+0.05, b.y1-b.y0-0.3)
+    bbox = Bbox.from_bounds(b.x0, b.y0-0.3, b.x1-b.x0, b.y1-b.y0)
+    plt.savefig(filename, bbox_inches=bbox)
